84b5ee558e446532fc0dbb708ce0eb6ffc384faa
Thomas Johnson <tjohnson1106@gmail.com>
2018-05-16 18:47:46 -0400

    created add text button and header and footer

---
+++ src/components/Main.js
@@ -0,0 +1,97 @@
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  Touc
+} from "react-native";
+
+import Touchable from "@appandflow/touchable";
+
+class Main extends Component {
+  state = {};
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Notes</Text>
+        </View>
+
+        <ScrollView style={styles.scrollContainer}>{}</ScrollView>
+
+        <View style={styles.footer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="note"
+            placeholderTextColor="white"
+            underlineColorAndroid="transparent"
+          />
+        </View>
+        <Touchable feedback="opacity" style={styles.addButton}>
+          <Text style={styles.addButtonText}>+</Text>
+        </Touchable>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  header: {
+    backgroundColor: "#E91E63",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 10,
+    borderBottomColor: "#ddd"
+  },
+  headerText: {
+    color: "white",
+    fontSize: 18,
+    padding: 26
+  },
+  scrollContainer: {
+    flex: 1,
+    marginBottom: 100
+  },
+
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10
+  },
+  textInput: {
+    alignSelf: "stretch",
+    color: "#fff",
+    padding: 20,
+    backgroundColor: "#252525",
+    borderTopWidth: 2,
+    borderTopColor: "#ededed"
+  },
+
+  addButton: {
+    position: "absolute",
+    backgroundColor: "#E91E63",
+    zIndex: 11,
+    right: 20,
+    bottom: 90,
+    height: 90,
+    width: 90,
+    height: 90,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 1
+  },
+  addButtonText: {
+    color: "#fff",
+    fontSize: 24
+  }
+});
+
+export default Main;
